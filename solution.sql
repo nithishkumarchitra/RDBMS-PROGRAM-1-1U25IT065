b8117fcaea1fb761b482b6e0c4385e9cc82ee0e9
nithishkumarchitra <nithishkumarchitra@gmail.com>
2026-08-20 13:39:35 +0530

    RDBMS PROGRAM 1 1U25IT065

--- solution.sql
+++ solution.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-CREATE TABLE Department (
-DepartmentID NUMBER(5) PRIMARY KEY,
-DepartmentName VARCHAR(20)
-HOD VARCHAR(20)
+CREATE DATABASE UniversityDB;
+USE universityDB;
+CREATE TABLE department(
+  DepartmentID numeric(5)PRIMARY KEY,
+  DepartmentName varchar(20) NOT NULL,
+  HOD varchar(20)NOT NULL
 );
-DESC Department;
+  desc Department;
